b08e7d32f7aad7dcd20069d3b32934580fb83cee
vsakkas <vasileios.sakkas96@gmail.com>
2023-11-07 20:20:35 +0200

    Add additional test

--- tests/test_ask.py
+++ tests/test_ask.py
@@ -7,3 +7,11 @@ from bard import BardClient
 async def test_ask() -> None:
     async with BardClient() as bard:
         _ = await bard.ask("Tell me a joke")
+
+
+@pytest.mark.asyncio
+async def test_ask_multiple_prompts() -> None:
+    async with BardClient() as bard:
+        _ = await bard.ask("Tell me a joke.")
+
+        _ = await bard.ask("Tell me another one.")
